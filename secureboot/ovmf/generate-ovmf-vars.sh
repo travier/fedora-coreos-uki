30b9368f7d4b03b173cb345dafc4b5fa3b8d23b3
jbtrystram <jbtrystram@redhat.com>
2024-09-10 23:52:31 +0200

    add a script to generate OVMF vars

---
+++ secureboot/ovmf/generate-ovmf-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GUID=$(cat ../GUID)
+
+virt-fw-vars --input /run/host/usr/share/edk2/ovmf/OVMF_VARS.secboot.fd \
+  --secure-boot  \
+  --set-pk $GUID ../keys/PK/PK.pem \
+  --add-kek $GUID ../keys/KEK/KEK.pem \
+  --add-db $GUID ../keys/db/db.pem \
+  -o VARS_CUSTOM.secboot.fd 
+
