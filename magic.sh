a7c2ff9bba9461cc850ba583ca8a314748a4e933
jbtrystram <jbtrystram@redhat.com>
2024-09-10 23:32:50 +0200

    add a fully automated script

--- magic.sh
+++ magic.sh
@@ -1,16 +1,14 @@
-# Fedora CoreOS UKI PoC
+#!/bin/bash
+set -euxo pipefail
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
-# How to reproduce
+echo "setting up systemd-boot..."
 
-## Get a secureboot VM
-See under secureboot
-
-just run `curl https://raw.githubusercontent.com/travier/fedora-coreos-uki/main/magic.sh | bash` as root.
-## 1 setup systemdboot
-
-```
-curl https://dl.fedoraproject.org/pub/fedora/linux/updates/40/Everything/x86_64/Packages/s/systemd-boot-unsigned-255.10-3.fc40.x86_64.rpm \ 
+curl https://dl.fedoraproject.org/pub/fedora/linux/updates/40/Everything/x86_64/Packages/s/systemd-boot-unsigned-255.10-3.fc40.x86_64.rpm \
   --output systemd-boot-unsigned-255.10-3.fc40.x86_64.rpm
 rpm-ostree usroverlay
 rpm -i systemd-boot-unsigned-255.10-3.fc40.x86_64.rpm
@@ -20,43 +18,26 @@ bootctl install
 # default sd-boot is 0 and default will boot firmware
 echo "timeout 30" >> /boot/efi/loader/loader.conf
 
-```
-## 2 Download signed composeFS fedora coreOS 
 
-Firstly, we enable fs-verity on the ostree repository
-```
-# turn on fsverity on filesystem
-sudo tune2fs -O verity /dev/vda4
-# enable fsverity in the ostree repo
+echo "Download signed composeFS fedora coreOS"
+
+tune2fs -O verity /dev/vda4
 ostree config set ex-fsverity.required true
   
-```
-
-Then, we download and deploy the ostree build 
-```
-# create a new mount namespace
 unshare -m
-
-# remount /sysroot to writeable
 mount -o remount,rw /sysroot
 
 osversion="$(skopeo inspect docker://quay.io/travier/fedora-coreos-uki:latest | jq -r '.Labels."org.opencontainers.image.version"')"
-# extract ostree commit from image to a new reference : fedora/coreos/uki/$version.
 ostree container unencapsulate \
   --repo /sysroot/ostree/repo \
   --write-ref "fedora/coreos/uki/${osversion}" \
   ostree-unverified-image:registry:quay.io/travier/fedora-coreos-uki:latest
 
-# deploy the ostree commit we just unencapsulated
 ostree admin deploy --stage "fedora/coreos/uki/${osversion}"
-
 ostree admin post-copy
 
-```
+echo "setting up the UKI"
 
-## Install the UKI
-```
-# container to extract the UKI
 podman create --name uki quay.io/travier/fedora-coreos-uki:uki
 podman cp uki:/uki uki
 podman cp uki:/systemd-bootx64-signed.efi systemd-bootx64-signed.efi
@@ -67,14 +48,9 @@ podman rmi quay.io/travier/fedora-coreos-uki:uki
 mount -o remount,rw /boot
 machine_id=$(cat /etc/machine-id)
 kernelver=$(uname -r)
-mv uki /boot/efi/EFI/Linux/$machine_id-$kernelver.efi
+mv uki /boot/efi/EFI/Linux/"$machine_id"-"$kernelver".efi
 
-# copy the signed systemd-boot
+echo "rewrite systemd-boot binary with the secureboot-signed one"
 mv systemd-bootx64-signed.efi /boot/efi/EFI/systemd/systemd-bootx64.efi
-```
-Reboot !
 
-See fsverity-success to check if all is working as expected.
-## License
-
-See [LICENSE](LICENSE) or [CC0](https://creativecommons.org/public-domain/cc0/).
+echo "All done, now you can just reboot"
